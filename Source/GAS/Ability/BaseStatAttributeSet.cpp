55d8031ec1d50d8c5830276932368a306d45ec52
donghyeon <gg6318@naver.com>
2023-01-01 12:56:26 +0900

    기본 AttributeSet 추가

--- Source/GAS/Ability/BaseStatAttributeSet.cpp
+++ Source/GAS/Ability/BaseStatAttributeSet.cpp
@@ -1,2 +1,28 @@
 #include "Ability/BaseStatAttributeSet.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
+#include "Ability/GASAbilitySystemComponent.h"
 
+void UBaseStatAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+}
+
+void UBaseStatAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+}
+
+void UBaseStatAttributeSet::AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty)
+{
+	UAbilitySystemComponent* ASC = GetOwningAbilitySystemComponent();
+	const float CurrentMaxValue = MaxAttribute.GetCurrentValue();
+	if (!FMath::IsNearlyEqual(CurrentMaxValue, NewMaxValue) && ASC)
+	{
+		// Change current value to maintain the current Val / Max percent
+		const float CurrentValue = AffectedAttribute.GetCurrentValue();
+		float NewDelta = (CurrentMaxValue > 0.f) ? (CurrentValue * NewMaxValue / CurrentMaxValue) - CurrentValue : NewMaxValue;
+
+		ASC->ApplyModToAttributeUnsafe(AffectedAttributeProperty, EGameplayModOp::Additive, NewDelta);
+	}
+}
--- Source/GAS/Ability/BaseStatAttributeSet.h
+++ Source/GAS/Ability/BaseStatAttributeSet.h
@@ -1,12 +1,70 @@
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "BaseStatAttributeSet.generated.h"
+
+// Uses macros from AttributeSet.h
+// The following example of the macro :
+// ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, Health)
+// will create the following functions :
+// static FGameplayAttribute GetHealthAttribute();
+// float GetHealth() const;
+// void SetHealth(float NewVal);
+// void InitHealth(float NewVal);
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName) 
 
 UCLASS()
 class GAS_API UBaseStatAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+protected:
+	void AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty);
+
+public:
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, Health)
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, MaxHealth)
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, Mana)
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, MaxMana)
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, AttackPower)
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, DefensePower)
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, MoveSpeed)
+	ATTRIBUTE_ACCESSORS(UBaseStatAttributeSet, Damage)
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData MaxMana;
+
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData MoveSpeed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData AttackPower;
+
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData DefensePower;
+
+	// 임시로 받아야할 데미지를 저장하는 변수
+	UPROPERTY(BlueprintReadOnly, Category = "BaseStat")
+	FGameplayAttributeData Damage;
 };
